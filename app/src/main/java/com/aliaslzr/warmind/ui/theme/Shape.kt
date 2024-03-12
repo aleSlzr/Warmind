a0d999df450f3f302fc7ad249f1188f67df87864
Alejandro Salazar <salazar.cruz.alejandro@gmail.com>
2024-03-12 17:10:07 -0600

    - adding theme colors and launcher icons

---
+++ app/src/main/java/com/aliaslzr/warmind/ui/theme/Shape.kt
@@ -0,0 +1,13 @@
+package com.aliaslzr.warmind.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(16.dp),
+    large = RoundedCornerShape(16.dp),
+    extraLarge = RoundedCornerShape(16.dp)
+)
